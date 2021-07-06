d03f77b2013ab392f59fef96f129b09ca3e69b58
kerasus <aliesmaeeli20@gmail.com>
2021-07-06 15:06:43 +0430

    Schedule page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <!-- App.vue -->
-
   <v-app>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app right :width="130">
       <!-- -->
+      <router-link to="/">Home</router-link> |
+      <router-link to="/Schedule">برنامه</router-link>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app elevate-on-scroll>
       <!-- -->
     </v-app-bar>
 
@@ -17,7 +18,7 @@
       <v-container fluid>
 
         <!-- If using vue-router -->
-        <router-view></router-view>
+        <router-view/>
       </v-container>
     </v-main>
 
@@ -25,13 +26,28 @@
       <!-- -->
     </v-footer>
   </v-app>
+
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
-};
-</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
