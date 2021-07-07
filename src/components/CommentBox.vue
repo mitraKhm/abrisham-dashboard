31e90cc6124a4033ffc5977ea99a3e5b85512c06
Alaa <maryamrazmjoo76@yahoo.com>
2021-07-07 15:59:55 +0430

    start side menu

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -1,7 +1,17 @@
 <template>
+  <div class="comment-box">
+    <v-textarea
+        class="rounded-xl  comment-main"
+        background-color="#eff3ff"
+        placeholder="یادداشت این جلسه"
+        filled
+        auto-grow
+        solo
+        flat
+    >
 
-  <div>
-    <i class="fi fi-rr-camera" />
+
+    </v-textarea>
   </div>
 </template>
 
@@ -11,6 +21,47 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.comment-box .comment-main{
+  margin: 0 70px 883px 25px;
+}
+
+
+.comment-box .comment-main textarea::-webkit-input-placeholder {
+  color: #3e5480 !important;
+  padding-top: 19px;
+  padding-right: 25px;
+  font-size: 16px;
+
+}
+
+.comment-box .comment-main textarea:-moz-placeholder { /* Firefox 18- */
+  color: #3e5480 !important;
+  padding-top: 19px;
+  padding-right: 25px;
+  font-size: 16px;
+}
+
+.comment-box .comment-main textarea::-moz-placeholder {  /* Firefox 19+ */
+  color: #3e5480 !important;
+  padding-top: 19px;
+  padding-right: 25px;
+  font-size: 16px;
+}
+
+.comment-box .comment-main textarea:-ms-input-placeholder {
+  color: #3e5480 !important;
+  padding-top: 19px;
+  padding-right: 25px;
+  font-size: 16px;
+}
+
+.comment-box .comment-main textarea::placeholder {
+  color: #3e5480 !important;
+  padding-top: 19px;
+  padding-right: 25px;
+  font-size: 16px;
+}
+
 
 </style>
