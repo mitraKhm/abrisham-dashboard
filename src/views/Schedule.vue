66c016657ea5df2848b7769596975df7b8070131
kerasus <aliesmaeeli20@gmail.com>
2021-07-07 10:34:17 +0430

    page layout

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="schedule-page">
+    <div>
+      نمایش محتوا بر اساس فعالیت شما
+      chip1
+      chip2
+    </div>
+    <v-row>
+      <v-col md="9">
+        <v-row>
+          <v-col>content-component</v-col>
+          <v-col>content-comment</v-col>
+        </v-row>
+      </v-col>
+      <v-col md="3">
+        <v-row>
+          <v-col>side-list-videos</v-col>
+          <v-col>side-list-pamphlet</v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </div>
 </template>
