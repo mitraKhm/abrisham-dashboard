b9549d6c2674d468742a4339356c42fc49eb4ebe
mitraKhm <mitra.kh1995@gmail.com>
2021-07-06 23:37:56 -0700

    chip box

--- src/components/chipGroup.vue
+++ src/components/chipGroup.vue
@@ -1,12 +1,58 @@
 <template>
-<div>
-  chip
-</div>
+  <div >
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: "chipGroup"
+  name: "chipGroup",
+  props: {
+    value: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            title: 'تجربی',
+            id: 1,
+            selected: false,
+            color: '#ffffff',
+          },
+          {
+            title: 'ریاضی',
+            id: 2,
+            selected: true,
+            color: 'blue',
+          }
+        ]
+      },
+    },
+    chipTitle: {
+      type: String,
+      default: 'رشته'
+    }
+
+  },
+  data() {
+    return {
+      chipData: [],
+    }
+  },
+
+  watch: {
+   value: {
+      handler() {
+       this.chipData=this.value
+        console.log('run')
+      },
+      immediate: true
+    },
+    chipData:{
+      handler(){
+
+      }
+    }
+  }
 }
 </script>
 
