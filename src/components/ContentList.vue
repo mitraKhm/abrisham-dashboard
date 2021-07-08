d982db767b8f80fd7c608ea7e4b8b674b15c4edb
mitraKhm <mitra.kh1995@gmail.com>
2021-07-08 03:33:07 -0700

    content list item done

--- src/components/ContentList.vue
+++ src/components/ContentList.vue
@@ -1,7 +1,38 @@
 <template>
   <div class="content-list-box">
-    <slot name="header" />
-    <slot name="filter" />
+    <slot name="header">
+      <div class="slot-header-box">
+        <v-chip
+          text-color="#3e5480"
+          depressed
+          color="transparent"
+          class="slot-header-box-movie"
+        >
+          فیلم های 14 تیر 1400
+        </v-chip>
+        <v-chip
+          text-color="#9fa5c0"
+          depressed
+          color="transparent"
+          class="slot-header-box-days"
+        >
+          رفتن به روزهای دیگر
+        </v-chip>
+      </div>
+    </slot>
+    <slot name="filter">
+      <v-select
+        :items="items"
+      >
+        <template v-slot:option="option">
+          <span
+            class="fa"
+            :class="option.icon"
+          />
+          {{ option.title }}
+        </template>
+      </v-select>
+    </slot>
     <div class="content-box">
       <content-list-item
         v-for="i in 5"
@@ -18,6 +49,11 @@ export default {
   components : {
     ContentListItem
   },
+  data(){
+    return {
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+    }
+  }
 }
 </script>
 
@@ -29,11 +65,28 @@ export default {
 .content-box {
   margin-top: 43px;
 }
+.slot-header-box{
+  display: flex;
+  justify-content: space-between;
+  margin: 19px 26px
+}
+.slot-header-box .slot-header-box-movie{
+  font-size: 20px;
+  height: 32px;
+  font-weight: 500;
+}
+.slot-header-box .slot-header-box-days{
+  font-size: 14px;
+}
+font-size: 12px;
 @media screen and (max-width: 1200px){
   .content-box {
     margin-top: 20px;
   }
-
+  .slot-header-box{
+    flex-direction: column;
+    margin: 13px 11px
+  }
 }
 @media screen and (max-width: 576px) {
   .content-box {
@@ -41,11 +94,22 @@ export default {
   }
 }
 @media screen and (max-width: 350px) {
-
+  .slot-header-box{
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 13px 11px
+  }
+  .slot-header-box .slot-header-box-movie {
+    font-size:16px;
+  }
+  .slot-header-box .slot-header-box-days{
+    font-size: 12px;
+  }
 }
 @media screen and (max-width: 320px) {
   .content-box {
     margin-top: 23px;
   }
+
 }
 </style>
