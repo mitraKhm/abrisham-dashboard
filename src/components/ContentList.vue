f168bb5065607d7bda9696a3e95990c707e3747a
mitraKhm <mitra.kh1995@gmail.com>
2021-07-07 23:17:38 -0700

    content list item

--- src/components/ContentList.vue
+++ src/components/ContentList.vue
@@ -2,10 +2,12 @@
   <div class="content-list-box">
     <slot name="header" />
     <slot name="filter" />
-    <content-list-item
-      v-for="i in 5"
-      :key="i"
-    />
+    <div class="content-box">
+      <content-list-item
+        v-for="i in 5"
+        :key="i"
+      />
+    </div>
   </div>
 </template>
 
@@ -24,6 +26,26 @@ export default {
   border-radius: 30px;
   border: solid 6px #eff3ff;
 }
+.content-box {
+  margin-top: 43px;
+}
+@media screen and (max-width: 1200px){
+  .content-box {
+    margin-top: 20px;
+  }
 
+}
+@media screen and (max-width: 576px) {
+  .content-box {
+    margin-top: 25px;
+  }
+}
+@media screen and (max-width: 350px) {
 
+}
+@media screen and (max-width: 320px) {
+  .content-box {
+    margin-top: 23px;
+  }
+}
 </style>
--- src/components/ContentListItem.vue
+++ src/components/ContentListItem.vue
@@ -1,24 +1,50 @@
 <template>
-  <div class="d-flex px-10">
-    <div class="contentListItem-box">
-      <v-sheet
-        width="96"
-        height="54"
-        color="red"
+  <div class="d-flex px-8">
+    <div>
+      <v-card
+        width="100%"
+        height="22"
+        class="mb-2 rounded-pill text-center text-caption"
+        flat
+        dark
+        color="#009498"
       >
-      </v-sheet>
-      <div
-        v-if="data[0].seen"
-        class="seen"
-      />
+    تلت
+      </v-card>
+      <div class="contentListItem-box">
+        <v-card
+          flat
+          class="rounded-card"
+        >
+          <v-img
+            src="../assets/hendese.jpg"
+          />
+        </v-card>
+        <div
+          v-if="data[0].seen"
+          class="seen"
+        />
+      </div>
     </div>
-    <div class="test">
-      <p class="contentListItem-title text-truncate">
-        {{ data[0].title }}
-      </p>
-      <p class="contentListItem-description text-truncate">
-        {{ data[0].description }}
-      </p>
+    <div class="mr-4">
+      <v-sheet
+        text-color="#3e5480"
+        depressed
+        height="22"
+        width="78"
+        class="mb-2 rounded-pill text-center text-caption"
+        color="#eff3ff"
+      >
+        افبقلادقفبلات
+      </v-sheet>
+      <div class="title-box">
+        <p class="contentListItem-title text-truncate">
+          {{ data[0].title }}
+        </p>
+        <p class="contentListItem-description text-truncate">
+          {{ data[0].description }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -36,7 +62,7 @@ export default {
             title: 'جلسه فیلان - فرسنگ هشتم - دین و زندگی',
             // eslint-disable-next-line vue/max-len
             description: ' (قسمت بیست و سوم)،   (قسمت بیست و سوم)، فصل سومفصل سوم',
-            seen: true,
+            seen: false,
           },
         ]
       }
@@ -48,9 +74,7 @@ export default {
 <style scoped>
 .contentListItem-box {
   position: relative;
-  margin-left: 15px;
 }
-
 .contentListItem-box .seen {
   height: 54px;
   width: 96px;
@@ -60,19 +84,74 @@ export default {
   position: absolute;
   top: 0;
 }
-.contentListItem-description{
+.contentListItem-description {
   font-size: 14px;
   color: #9fa5c0;
 }
 .contentListItem-title {
- background-color: red;
   font-size: 18px;
   font-weight: 500;
   text-align: right;
   color: #3e5480;
   margin-bottom: 0;
 }
-.test{
-  background-color: yellow;
+.title-box {
+  width: calc(100% - 130px);
+}
+.contentListItem-box .rounded-card{
+  width: 96px;
+  height: 54px;
+  border-radius: 10px;
+}
+@media screen and (max-width: 1200px){
+  .contentListItem-box {
+    position: relative;
+  }
+  .contentListItem-box .rounded-card{
+    height: 40px;
+    width: 71px;
+    border-radius: 5px;
+  }
+  .contentListItem-box .seen {
+    height: 40px;
+    width: 71px;
+    border-radius: 5px;
+  }
+  .contentListItem-description {
+    font-size: 14px;
+    color: #9fa5c0;
+  }
+  .title-box {
+    width: calc(100% - 130px);
+  }
+ .title-box .contentListItem-title {
+    font-size: 18px;
+    font-weight: 500;
+    text-align: right;
+    color: #3e5480;
+    margin-bottom: 0;
+  }
+}
+@media screen and (max-width: 576px) {
+
+}
+@media screen and (max-width: 350px) {
+
+}
+@media screen and (max-width: 320px) {
+  .contentListItem-box .rounded-card{
+    height: 33px;
+    width: 60px;
+    border-radius: 15px;
+  }
+  .contentListItem-box .seen {
+    height: 33px;
+    width: 60px;
+    border-radius: 15px;
+  }
+  .title-box .contentListItem-title{
+    font-size: 14px;
+  }
+
 }
 </style>
