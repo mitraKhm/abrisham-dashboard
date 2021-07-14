ee9fe0a29fcd064ace3197af13aa2e275b39f55f
Alaa <maryamrazmjoo76@yahoo.com>
2021-07-14 17:16:57 +0430

    bookmark and seen button completed

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
-import VueSocialSharing from 'vue-social-sharing'
 
 Vue.config.productionTip = false
-Vue.use(VueSocialSharing)
 
 new Vue({
   vuetify,
