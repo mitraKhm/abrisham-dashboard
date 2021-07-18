209eb3dcbf47f29ee880ecfcc18470227933580d
Alaa <maryamrazmjoo76@yahoo.com>
2021-07-18 14:22:15 +0430

    coming soon's images was add to assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,14 @@
     <img
       alt="coming soon"
       class="coming-soon-image"
+      src="../assets/image/cs_960-1920.png"
     >
+    <p class="coming-soon">
+      ...به زودی
+    </p>
+    <p class="coming-soon-description">
+      این بخش هنوز آماده نیست ولی به زودی آماده و فعال میشه.
+    </p>
   </div>
 </template>
 
@@ -19,7 +26,30 @@ export default {
 </script>
 <style scoped>
 .home .coming-soon-image{
-  
+  width: 873px;
+  height: 454px;
+  margin: 114px 477px 43px 440px;
+}
+.home .coming-soon{
+  text-align: center;
+  font-size: 40px;
+  font-weight: 500;
+  color: #3e5480;
+}
+.home .coming-soon-description{
+  font-size: 20px;
+  font-weight: 500;
+  text-align: center;
+  color: #9fa5c0;
+}
+
+@media screen and (max-width: 1200px) {
+  .home .coming-soon-image{
+    width: 478px;
+    height: 34px;
+    margin: 97px 311px 113px;
+  }
+
 }
 
 </style>
