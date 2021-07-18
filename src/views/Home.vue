2ed462f116b0b2e7e3df90288c8e4f3e051ae367
Alaa <maryamrazmjoo76@yahoo.com>
2021-07-18 13:10:27 +0430

    coming soon's images was add to assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="home">
     <img
-      alt="Vue logo"
-      src="../assets/logo.png"
+      alt="coming soon"
+      class="coming-soon-image"
     >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+<style scoped>
+.home .coming-soon-image{
+  
+}
+
+</style>
