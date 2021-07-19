d23b709c512e4aa3973ba1468f8b742bdf67ee50
kerasus <aliesmaeeli20@gmail.com>
2021-07-19 14:46:41 +0430

    syncCurrentContentWithContentInList

--- src/Mixin/ContentMixin.js
+++ src/Mixin/ContentMixin.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import {Content} from '../Models/Content'
 
 const ContentMixin = {
     methods: {
+        syncCurrentContentWithContentInList () {
+            let targetContentIndex = this.contents.list.indexOf( item => item.id === this.currentContent.id)
+            if (!targetContentIndex) {
+                return false
+            }
+
+            this.contents.list[targetContentIndex] = new Content(this.currentContent)
+        },
         toggleFavor () {
             this.currentContent.loading = true
             if (this.currentContent.is_favored) {
@@ -19,6 +28,7 @@ const ContentMixin = {
                 .then( () => {
                     this.currentContent.has_watched = true
                     this.currentContent.loading = false
+                    this.syncCurrentContentWithContentInList()
                 })
                 .catch( () => {
                     this.currentContent.loading = false
@@ -29,6 +39,7 @@ const ContentMixin = {
                 .then( () => {
                     this.currentContent.is_favored = true
                     this.currentContent.loading = false
+                    this.syncCurrentContentWithContentInList()
                 })
                 .catch( () => {
                     this.currentContent.loading = false
@@ -39,6 +50,7 @@ const ContentMixin = {
                 .then( () => {
                     this.currentContent.is_favored = false
                     this.currentContent.loading = false
+                    this.syncCurrentContentWithContentInList()
                 })
                 .catch( () => {
                     this.currentContent.loading = false
@@ -53,6 +65,7 @@ const ContentMixin = {
                     .then(response => {
                         this.currentContent.comments[0].comment = response.data.data.comment
                         this.comment = this.currentContent.comments[0].comment
+                        this.syncCurrentContentWithContentInList()
                     })
             } else {
                 axios.post('/api/v2/comment', {
@@ -63,6 +76,7 @@ const ContentMixin = {
                     .then(response => {
                         this.currentContent.comments.push({ comment: response.data.data.comment })
                         this.comment = this.currentContent.comments[0].comment
+                        this.syncCurrentContentWithContentInList()
                     })
             }
         },
